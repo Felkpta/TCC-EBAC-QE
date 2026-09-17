9fc70fbfa2588f2500d65df382a06f5d98412007
Felkpta <cruzeiro933@gmail.com>
2026-09-17 09:47:36 -0300

    test(ui): finaliza automacao da US-0002 e adiciona gitignore

---
+++ UI/cypress/e2e/login.cy.js
@@ -0,0 +1,17 @@
+﻿import loginPage from '../support/page-objects/login.page'
+
+describe('US-0002: Funcionalidade de Login', () => {
+  beforeEach(() => {
+    loginPage.visitarUrl()
+  })
+
+  it('Deve realizar login com sucesso (Caminho Feliz)', () => {
+    loginPage.preencherLogin('aluno_ebac@teste.com', 'teste@teste.com')
+    cy.get('.woocommerce-MyAccount-content').should('be.visible')
+  })
+
+  it('Deve exibir mensagem de erro ao inserir senha inválida (Caminho Alternativo)', () => {
+    loginPage.preencherLogin('aluno_ebac@teste.com', 'senha_incorreta')
+    cy.get('.woocommerce-error').should('be.visible')
+  })
+})
---
+++ UI/cypress/support/page-objects/login.page.js
@@ -0,0 +1,17 @@
+﻿class LoginPage {
+  get #inputUser() { return cy.get('#username') }
+  get #inputPassword() { return cy.get('#password') }
+  get #btnLogin() { return cy.get('[name="login"]') }
+
+  visitarUrl() {
+    cy.visit('minha-conta')
+  }
+
+  preencherLogin(usuario, senha) {
+    this.#inputUser.clear().type(usuario)
+    this.#inputPassword.clear().type(senha, { log: false })
+    this.#btnLogin.click()
+  }
+}
+
+export default new LoginPage()
